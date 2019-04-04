db7958454f9d83bed6e7f6fea4b84ebc9751808b
dsantos234 <ddelossantosmateo@gmail.com>
2019-04-04 18:46:15 -0400

    Displaying changes of news

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { API, API_KEY, DEFAULT } from "../routes/NewsAPI";
 import { any } from "prop-types";
-import {Card, Col, Row} from 'antd';
+import { Card, Col, Row, Icon } from 'antd';
+
+const { Meta } = Card;
+
 
 export default class Start extends React.Component<any, any> {
   constructor(props: any) {
@@ -16,27 +19,30 @@ export default class Start extends React.Component<any, any> {
 
 
   public componentDidMount() {
-      fetch(API + DEFAULT + '&apiKey=' + API_KEY).then(res => res.json()).then((response) => {
-        this.setState({results: response.articles})
-        console.log(response.articles);
-      },
+    fetch(API + DEFAULT + '&apiKey=' + API_KEY).then(res => res.json()).then((response) => {
+      this.setState({ results: response.articles, isLoaded: true })
+      console.log(response.articles);
+    },
       (error) => {
-        this.setState({isLoaded: true, error: error})
+        this.setState({ isLoaded: true, error: error })
       }
-      )
-    }
-
+    )
+  }
   public render() {
-    const{error, isLoaded, results} = this.state;
+    const { error, isLoaded, results } = this.state;
     if (error) {
       return <div><h2>Error: {error.message}</h2></div>
     } else if (!isLoaded) {
       return <div><h2>Loading...</h2></div>
     } else {
-      return(
+      return (
         <div>
-          {results.map((result: any) => (
-            <div>result</div>
+          {results.map((result: any, index: any) => (
+            <div key={index}>
+              <Card actions={[<Icon type="heart" theme="outlined"/>]} cover={<img alt="news_image" src={result.urlToImage}/>}>
+                <Meta title={result.title} description={result.description}/>
+                {result.content}
+              </Card></div>
           ))}
         </div>
       );
